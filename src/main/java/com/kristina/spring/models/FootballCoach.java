e0ba777923d417f4203f4f2db42fed6095c505d1
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2021-05-14 15:07:23 +0200

    Created a  literal value injection example

--- src/main/java/com/kristina/spring/models/FootballCoach.java
+++ src/main/java/com/kristina/spring/models/FootballCoach.java
@@ -5,10 +5,15 @@ import com.kristina.spring.services.SportsMotivationService;
 public class FootballCoach implements Coach{
     private SportsMotivationService motivationService;
 
+
+
+    private String emailAddress;
+    private String teamName;
+
     public FootballCoach(){
         System.out.println("Football coach was created.");
     }
-    
+
     @Override
     public void adviceClient() {
         motivationService.createTrainingPlan();
@@ -16,5 +21,17 @@ public class FootballCoach implements Coach{
 
     public void setMotivationService(SportsMotivationService motivationService) {
         this.motivationService = motivationService;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
     }
 }
