6ec32d36b5323b0231098913ccc4d07be06e559f
Kristina Cerniauskaite <k.cerniauskaite@gmail.com>
2021-07-13 16:25:15 +0200

    Updated app config for autowiring

--- src/main/java/com/kristina/tekstac/autowiring/ApplicationConfig.java
+++ src/main/java/com/kristina/tekstac/autowiring/ApplicationConfig.java
@@ -1,18 +1,9 @@
 package com.kristina.tekstac.autowiring;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 public class ApplicationConfig {
-    @Bean
-    public Passport passport() {
-        return new Passport();
-    }
-    @Bean
-    public Employee employee(){
-        return new Employee();
-    }
 }
